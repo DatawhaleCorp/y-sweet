90ea60b502217a5e8a705143a40a137bed87be65
ChanWunsam <ChanWunsam@gmail.com>
2025-02-08 15:33:55 +0800

    chore: 连接y-sweet本地服务器不使用广播地址，而是127.0.0.1

--- run.sh
+++ run.sh
@@ -7,4 +7,4 @@ docker build --build-arg ALI_ACCESS_KEY=$(grep ALI_ACCESS_KEY .env | cut -d '=' 
 docker run --env-file .env -d -p 8080:8080 --name y-sweet-container y-sweet-image
 
 cd examples/nextjs
-CONNECTION_STRING=ys://0.0.0.0:8080 npm run dev
+CONNECTION_STRING=ys://127.0.0.1:8080 npm run dev
